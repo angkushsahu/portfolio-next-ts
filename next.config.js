988224bf9e88a66fdad99bfb867e78dbaed0b5f5
Angkush <angkysahu2502@gmail.com>
2022-08-22 03:26:41 +0530

    removed pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa");
 
-const nextConfig = withPWA({
+const nextConfig = {
 	reactStrictMode: true,
 	images: {
 		domains: ["res.cloudinary.com"],
@@ -12,13 +11,6 @@ const nextConfig = withPWA({
 		PUBLIC_KEY: "p-N4YX4VvkVQ4lxz4",
 		FORMSPREE_ID: "xjvlnwka",
 	},
-	pwa: {
-		dest: "public",
-		register: true,
-		swSrc: "sw.js",
-		skipWaiting: true,
-		disable: process.env.NODE_ENV === "development",
-	},
-});
+};
 
 module.exports = nextConfig;
